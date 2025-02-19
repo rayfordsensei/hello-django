506b98303e230e2877a3bea416331109f5e61d44
rayfordsensei <41239623+rayfordsensei@users.noreply.github.com>
2025-02-19 20:08:26 +0300

    initial release of hello app

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# pyright: reportArgumentType=false
+
+from django.http import HttpResponse
+
+# from django.shortcuts import render
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world!")
--- helloDjango/settings.py
+++ helloDjango/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "hello",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
