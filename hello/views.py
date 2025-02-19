74ba0a54a4c410b929ba4e40fb72b60793a02c7d
rayfordsensei <41239623+rayfordsensei@users.noreply.github.com>
2025-02-19 20:26:32 +0300

    test templates

--- hello/views.py
+++ hello/views.py
@@ -1,14 +1,19 @@
 # pyright: reportArgumentType=false
 
-from django.http import HttpResponse
-
-# from django.shortcuts import render
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")
 
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(
+        request,
+        "hello/greet.html",
+        {
+            "name": name.capitalize(),
+        },
+    )
