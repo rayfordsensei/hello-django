ef7ef432af7d859248aca2df593dd99bb2b288b9
rayfordsensei <41239623+rayfordsensei@users.noreply.github.com>
2025-02-19 20:13:55 +0300

    test custom route

--- hello/views.py
+++ hello/views.py
@@ -8,3 +8,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world!")
+
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
