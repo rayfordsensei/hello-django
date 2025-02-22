465b6e0cebbbce2ea94205d766b5eba3d67c3d21
rayfordsensei <41239623+rayfordsensei@users.noreply.github.com>
2025-02-22 21:50:03 +0300

    add Flight model; update db; fix project settings

--- flights/models.py
+++ flights/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
+
+class Flight(models.Model):
+    origin = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,  # cascade deletes corresponding values aswell (flights).
+        related_name="departures",
+    )
+    destination = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="arrivals",
+    )
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"  # pyright:ignore[reportAttributeAccessIssue]
--- helloDjango/settings.py
+++ helloDjango/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "hello",
     "newyear",
     "tasks",
+    "flights",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
