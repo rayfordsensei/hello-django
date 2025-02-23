f5f86d080df9395a6d29c9cd76875807124f5061
rayfordsensei <41239623+rayfordsensei@users.noreply.github.com>
2025-02-23 17:28:36 +0300

    add more info to flight admin panel

--- flights/admin.py
+++ flights/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from .models import Airport, Flight, Passenger
 
+
 # Register your models here.
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
+
 admin.site.register(Airport)
-admin.site.register(Flight)
+admin.site.register(Flight, FlightAdmin)
 admin.site.register(Passenger)
